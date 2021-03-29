c4f5729ac3ad4d395494fae84242e6e3c598039c
KoheiYo <79289611+KoheiYo@users.noreply.github.com>
2021-03-29 19:02:38 +0900

    ；

--- JavaStudy/Check1/Check.java
+++ JavaStudy/Check1/Check.java
@@ -1,40 +1,43 @@
-/**
- * バブルソート
- * チェックテスト-Java1章
- *
- */
-public class Check{
-  public static void main(String[] args){
-      /*
-      * 問1
-      * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
-      */
-        int[]data = {3,1,2,7,5};
-      /*
-      * 問2
-      * 以下のfor文を完成させなさい
-      */
-      for(int i = 0; i < data.length; i++){
-          System.out.print(data[i] + " ");
-      }
+package study;
+
+public class Main {public static void main(String[] args) {
+
+    //① firstNameとlastNameという名前の変数を定義し、
+    //   firstNameには自分の名前、lastNameには自分の名字で初期化しなさい
+      String firstName = "Kohei";
+      String lastName = "Yoshino";
+      
+
+      //getName関数の呼び出しと出力
+      System.out.println(getName(firstName, lastName));
       System.out.println();
 
-      
-      for (int i = 0; i < data.length-1; i++) {
-          for (int j = data.length-1; j > i; j--) {
-              /*
-              * 問3
-              * 以下、配列の添字を入れてソートを完成させなさい
-              */
-              if(data[j-1] > data[j]){
-                int box = data[j];
-                data[j] = data[j-1];
-                data[j-1] = box;
-              }
-          }
-      }
-      for(int i = 0; i < data.length; i++){
-          System.out.print(data[i] + " ");
+      int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+      //③ arr 配列をループさせ、isOdd関数を使って配列の要素が奇数かどうかの判定を行いなさい。
+      //   要素が奇数の場合には「nは奇数です。」と出力されます。
+        for (int num= 0; num<= 9; num++) {
+        isOdd(num);
+		}
+
+
+  }
+
+  //② 関数を定義しなさい。
+  //   関数名：getName
+  //  修飾子：public
+  //   引数には①で定義したfirstNameとlastNameを引数で受け取り、
+  //  last_name と first_name を連結した値を返り値とする変数を定義しなさい。
+    public static String getName (String firstName, String lastName) {
+	return firstName + lastName;
+  }
+
+
+
+  //奇数の判定を行う。
+  public static void isOdd(int num) {
+    if(num % 2 != 0) {
+      System.out.println(num + "は奇数です。");
       }
   }
 }
